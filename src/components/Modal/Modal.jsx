d8aa32deca2bd1fdf9834438f8fdd22852d4c6f6
Shahriar Swim <shahriar2k18@gmail.com>
2021-12-08 17:02:10 +0600

    enter, exit animation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,10 +2,20 @@ import ModalWrapper from './ModalWrapper';
 import './Modal.css';
 
 const Modal = ({ setIsModalOpen }) => {
+  const onModalCloseHandler = () => {
+    const modalEl = document.querySelector('.modal');
+    modalEl.classList.remove('enter');
+    modalEl.classList.add('exit');
+
+    setTimeout(() => {
+      setIsModalOpen(false);
+    }, 300);
+  };
+
   return (
     <ModalWrapper>
-      <div className="modal">
-        <button onClick={() => setIsModalOpen(false)}>Close Modal</button>
+      <div className="modal enter">
+        <button onClick={onModalCloseHandler}>Close Modal</button>
       </div>
     </ModalWrapper>
   );
