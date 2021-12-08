5ec7a5178f08ee7706ce61262e7fd34a84d0f3c3
Shahriar Swim <shahriar2k18@gmail.com>
2021-12-08 23:14:05 +0600

    overlay click

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -14,6 +14,7 @@ const Modal = ({ setIsModalOpen }) => {
 
   return (
     <ModalWrapper>
+      <div className="overlay" onClick={onModalCloseHandler}></div>
       <div className="modal enter">
         <button onClick={onModalCloseHandler}>Close Modal</button>
       </div>
